01d29d81b11c5aa59f186c0b6825b1ff20fa2fcc
Áfia Araújo <afiaaraujo6@gmail.com>
2026-07-20 12:56:07 -0300

    fix: add cors policy for frontend origin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { app } from './app';
 import { env } from './config/env';
+import cors from 'cors';
+
+app.use(cors({
+  origin: 'https://sinutre-frontend-phi.vercel.app'
+}));
 
 app.listen(env.port, () => {
   console.log(`SiNutre back rodando em http://localhost:${env.port}`);
